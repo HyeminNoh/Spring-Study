38317d33be789e26228d441f808392d2db2e7c27
HyeminNoh <shrmffkfk@naver.com>
2023-02-17 08:40:15 +0900

    [UPDATE] 스프링 핵심원리 - AppConfig 리팩토링

--- Project/core/src/main/java/com/example/core/AppConfig.java
+++ Project/core/src/main/java/com/example/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.core;
 
+import com.example.core.discount.DiscountPolicy;
 import com.example.core.discount.FixDiscountPolicy;
+import com.example.core.member.MemberRepository;
 import com.example.core.member.MemberService;
 import com.example.core.member.MemberServiceImpl;
 import com.example.core.member.MemoryMemberRepository;
@@ -8,14 +10,25 @@ import com.example.core.order.OrderService;
 import com.example.core.order.OrderServiceImpl;
 
 // 애플리케이션의 전체 동작 방식을 구성하기 위해, 구현 객체를 생성하고 연결하는 책임을 가지는 별도의 설정 클래스
-// 관심사를 분리해주는 역할
+// 관심사를 분리해주는 역할 > 역할과 구현을 분리
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // MemoryMemberRepository를 사용하는 MemberServiceImpl
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(
+                memberRepository(),
+                discountPolicy());
     }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
 }
