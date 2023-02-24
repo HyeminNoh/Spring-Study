cef0932fc72bf516d5d591f62361452684302bb5
HyeminNoh <shrmffkfk@naver.com>
2023-02-24 11:10:49 +0900

    [ADD] 스프링 핵심원리 기본 - 컴포넌트 스캔

--- Project/core/src/main/java/com/example/core/AutoAppConfig.java
+++ Project/core/src/main/java/com/example/core/AutoAppConfig.java
@@ -2,8 +2,14 @@ package com.example.core;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan
+// @Component가 붙은걸 다 자동등록해줌
+@ComponentScan (
+        basePackages = "com.example.core",
+        // @Configuration 내부에 @Component 선언되어 있어서 자동 주입 제외를 위해 설정 (configuration 예제들이 많아서 테스트 파일들을 위해..)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
 public class AutoAppConfig {
 }
--- Project/core/src/main/java/com/example/core/member/MemberServiceImpl.java
+++ Project/core/src/main/java/com/example/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package com.example.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository; // 추상화에만 의존
 
+    @Autowired // 자동 의존 관계 주입 (생성자에 붙여주면 의존관계를 자동으로 주입하게 됨)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
