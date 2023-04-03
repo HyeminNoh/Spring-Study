c3c5f4e1b33afdf2c5cbd050069952ae390b8cf7
HyeminNoh <shrmffkfk@naver.com>
2023-04-03 10:51:25 +0900

    [ADD] 실전!스프링 부트와 JPA 활용 1 - 예제 프로젝트 환경설정

---
+++ Project/core/src/main/java/com/example/core/web/LogDemoController.java
@@ -0,0 +1,33 @@
+package com.example.core.web;
+
+import com.example.core.common.MyLogger;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody // view 화면 없이 문자를 바로 반환
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        // Thread.sleep(1000);
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
